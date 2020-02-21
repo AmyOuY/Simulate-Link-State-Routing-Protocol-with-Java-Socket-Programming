df384dfb0673eb42ce02681e5d2dd913fb6cf99d
YimiaoOu <32688730+YimiaoOu@users.noreply.github.com>
2020-02-20 20:47:15 -0500

    Update RouterStatus.java

--- node/RouterStatus.java
+++ node/RouterStatus.java
@@ -1,11 +1,10 @@
 package socs.network.node;
 
-/**
- * status of the router, in this simplified version of routing protocol, we only define INIT and
- * TWO_WAY for database synchronization, you can choose to keep other status by adding more fields
- * in router class or you can add more options here
- */
+
+/** status of the router, in this simplified version of routing protocol, we only define INIT and
+ TWO_WAY for database synchronization, you can choose to keep other status by adding more fields
+ in router class or you can add more options here */
 public enum RouterStatus {
-  INIT,
-  TWO_WAY,
+    INIT,
+    TWO_WAY,
 }
